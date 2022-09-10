8c256d9374b372ed6fa41441e0d883029e0ff365
freewindz01 <111021844+freewindz01@users.noreply.github.com>
2022-09-09 21:45:09 -0700

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,42 +1,39 @@
+#include <stdio.h>
 /**
- *main - main block
- *Description: Print all possible different combinations of 3 digits.
- *Numbers must be separated by commas and a space.
- *The 3 digits must be different.
- *012, 120, 102, 021, 201, 210 are considered the same combination.
- *print only the smallest combination of 3 digits.
- *Numbers should be printed in ascending order.
- *You can only use `putchar` to print to console.
- *You can only use `putchar` up to 6 times.
- *You are not allowed to use any variables of type `char`.
- *Return: 0
+ * main - main block
+ * Description: Print all possible different combinations of 3 digits.
+ * Numbers must be separated by commas and a space.
+ * The 3 digits must be different.
+ * 012, 120, 102, 021, 201, 210 are considered the same combination.
+ * print only the smallest combination of 3 digits.
+ * Numbers should be printed in ascending order.
+ * You can only use `putchar` to print to console.
+ * You can only use `putchar` up to 6 times.
+ * You are not allowed to use any variables of type `char`.
+ * Return: 0
  */
 int main(void)
 {
-	int i = 0, j;
-	
-	while (i <= 99)
+	int i, j, k, l;
+
+	for (i = 0; i < 1000; i++)
 	{
-		j = i;
-		while (j <= 99)
+		j = i / 100; /* hundreds */
+		k = (i / 10) % 10; /* tens */
+		l = i % 10; /* singles */
+
+		if (j < k && k < l)
 		{
-			if (j != i)
+			putchar(j + '0');
+			putchar(k + '0');
+			putchar(l + '0');
+
+			if (i < 700)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
-				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				
-				if (i != 98 || j != 98)
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				putchar(44);
+				putchar(32);
 			}
-			++j;
 		}
-		++i;
 	}
 	putchar('\n');
 
