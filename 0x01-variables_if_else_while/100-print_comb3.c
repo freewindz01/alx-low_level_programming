6da6e844e27d53f4a07f2c95316831e5cf0c9898
Ejembi_Sunday <s.ejembi@gmail.com>
2022-09-09 19:37:59 -0700

    print comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,28 +13,23 @@
  */
 int main(void)
 {
-	int i, j, k;
+	int i;
 
-	i = 0;
 
-	while (i < 100)
+	for (i = 0; i < 100; i++)
 	{
-		j = i % 10; /* singles digit */
-		k = i / 10; /* doubles digit */
+		putchar((i % 10) + '0'
+); 
+		putchar((i / 10) + '0'
+);
 
-		if (k < j)
+		if (i != 99)
 		{
-			putchar(k + '0');
-			putchar(j + '0');
+			putchar(',');
+			putchar(' ');
 
-			if (i + 89)
-			{
-				putchar(44);
-				putchar(32);
-			}
 		}
 
-		i++;
 	}
 	putchar('\n');
 
