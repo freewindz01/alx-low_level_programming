db8082bc02959836b419760f7b10b1fc1e18ca9c
Ejembi_Sunday <s.ejembi@gmail.com>
2022-09-11 23:25:44 -0700

    put.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,10 @@
- #include <stdio.h>
+#include <stdio.h>
 /**
-<<<<<<< HEAD
  * main - main block
  * Return: 0
  */
-=======
-* main - main block
-* Return: 0
-*/
->>>>>>> bb0c6267eac9d67f26cb8d9ce67840cd4150a689
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
