6791819a921d54971e98c503317f4ba7dee08f90
Ejembi_Sunday <s.ejembi@gmail.com>
2022-09-15 10:25:10 -0700

    a function that multiplies two integers.

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
-int mul(int a, int b);
+int mul(int x, int y);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
