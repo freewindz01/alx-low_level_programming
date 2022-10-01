75c5abc85a74f19cbcad3ae9ac8eebd5f55e63f3
Ejembi_Sunday <s.ejembi@gmail.com>
2022-09-30 17:36:01 -0700

    task 0

---
+++ 0x09-static_libraries/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - Copy a string starting from index 0 of `dest`.
+ * @dest: string
+ * @src: string
+ * @n: number of chars to copy over
+ * Return: `dest` edited string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int x;
+
+	for (x = 0; x < n && src[x] != '\0'; x++)
+		dest[x] = src[x];
+
+	for (; n > x; x++)
+		dest[x] = '\0';
+
+	return (dest);
+}
