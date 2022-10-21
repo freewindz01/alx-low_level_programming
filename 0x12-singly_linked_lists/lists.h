e6fb2ff81598ae5b3572cc62f2c6201c575ec73d
Ejembi_Sunday <s.ejembi@gmail.com>
2022-10-20 17:19:56 -0700

    task 0

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,9 @@
-#ifndef LINKED
-#define LINKED
+#ifndef LIST
+#define LIST
 
 #include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 
 /**
  * struct list_s - singly linked list
@@ -26,5 +25,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
-#endif /*LINKED*/
+#endif
