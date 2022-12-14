593adf67d2556ead84751df9b7b9443202c29878
Ejembi_Sunday <s.ejembi@gmail.com>
2022-12-13 17:07:12 -0800

    task 0

---
+++ 0x18-dynamic_libraries/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings
+ * @dest: string
+ * @src: string
+ * @n: number of elements to concatenate in
+ * Return: pointer to resulting `dest`
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+		;
+
+	for (b = 0; src[b] != '\0' && n > 0; b++, n--, a++)
+	{
+		dest[a] = src[b];
+	}
+	return (dest);
+}
